40b3820a3ff13113fbc68850e5622de525c20d2b
csinrn <csinrn24@gmail.com>
2020-10-20 18:46:07 +0800

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const request = require('request')
+const axios = require('axios')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',  (req, res, next) => {
+
+    var webhookURL = "https://9y8wy7m8tc.execute-api.us-east-2.amazonaws.com/webhook";
+    var date = new Date().toDateString();
+    console.log(date)
+    axios.post(
+        webhookURL, date
+    ).then((response)=>{           
+        //console.log(err, body)    
+        res.send(response.data)
+    }).catch((error) => { console.error(error) });
+});
+
+app.listen(port, function () {
+    console.log('Express app started on ' + port);
+});
