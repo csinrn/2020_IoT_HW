0ed36dde956d745781fae1b88f45c150f9e7261e
csinrn <csinrn24@gmail.com>
2020-10-29 22:43:26 +0800

    ft: add comments

--- HW3/mapper.py
+++ HW3/mapper.py
@@ -1,11 +1,12 @@
 import sys 
 
-#que = []
 for line in sys.stdin:
+    # remove the spaces
     line = line.strip()
+    # split the log with ' '
     words = line.split(' ')
+    # split out the 'date/month/year hour' format
     datehour = words[3].split(':')
+    # print the key-value pair
     datehour = datehour[0][1:] + '-' + datehour[1] + ":00:00"  # 12/Mar/2004-12:00:00
-    # que.append( (datehour, 1) )
     print "%s %s" % (datehour, 1)
-#print(que)
--- HW3/reducer.py
+++ HW3/reducer.py
@@ -3,9 +3,12 @@ import sys
 num = 0
 lastkey = ''
 for keypair in sys.stdin:
+    # split the key-value pair
     key, val = keypair.split(' ')
+    # count the sorted logs
     if key != lastkey:
         if lastkey != '':
+            # output
             print "%s    %s" % (key, num)
         lastkey = key
         num = 1
