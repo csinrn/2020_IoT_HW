a915b477cabc8e7717965786a8b6f983752f0c20
csinrn <csinrn24@gmail.com>
2020-11-26 13:40:39 +0800

    ft: add logistic_spark

--- HW4/myLogisticClass_spark.py
+++ HW4/myLogisticClass_spark.py
@@ -1,29 +1,26 @@
 import numpy
 import math
 import numpy as np
+import pyspark
 
 class myLogistic:
-    def __init__(self, lr, epoch_n, batch_n, feature_len):
+    def __init__(self, lr, epoch_n, feature_len):
         self.lr = lr
-        self.epoch_n = epoch_n    
-        self.batch_n = batch_n    
+        self.epoch_n = epoch_n
         self.w = np.zeros((feature_len,)) + 1
         self.b = 1
         self.feature_len = feature_len
 
     def train(self, dataset):
-        np.random.seed(123)
-        features = dataset[:,:self.feature_len]
-        labels = dataset[:, self.feature_len]
         for i in range(self.epoch_n):
-            indices = np.random.randint(len(features), size=(self.batch_n,))
-            features_e = features[indices]
-            labels_e = labels[indices]
-            dw, db = self.train_one_epoch(features_e, labels_e)
+            features_e = dataset.sample(False, 0.5, None)
+            dw, db = features_e.map(lambda x: self.train_one_epoch(x))
             self.w = self.w - sum(dw) / self.batch_n
             self.b = self.b - sum(db) / self.batch_n
 
-    def train_one_epoch(self, feature, label):
+    def train_one_epoch(self, data):
+        feature = data.take(range(0,self.feature_len))
+        label = data.item(self.feature_len)
         y_ = self.train_predict(feature)
         err = label - y_
         db = self.lr * err[:,None]
@@ -32,7 +29,7 @@ class myLogistic:
 
     def train_predict(self, feature):
         x = np.dot(feature, self.w) + self.b
-        return 1 / (1 + np.e**(x)) 
-    
+        return 1 / (1 + np.e**(x))
+
     def predict(self, feature):
-        return 1 if self.train_predict(feature) >= 0.5 else 0 
+        return 1 if self.train_predict(feature) >= 0.5 else 0
