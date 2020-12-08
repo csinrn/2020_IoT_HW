8d982c5f654397826a621991a0af46abb53349b6
csinrn <csinrn24@gmail.com>
2020-12-08 11:31:18 +0800

    ft: iot hw5

--- HW5/server.js
+++ HW5/server.js
@@ -6,12 +6,10 @@ const MongoClient = require('mongodb').MongoClient
 const objectID = require('mongodb').ObjectID
 
 var url = 'mongodb://root:27017';
-//var url = 'mongodb://127.0.0.1:27017/hw2';
 
-// 設定預設port為 1377，若系統環境有設定port值，則以系統環境為主
 app.set('port', (1377))
 
-// 設定靜態資料夾
+// set static folder
 app.use(express.static('/hw/public'))
 
 // parse application/x-www-form-urlencoded
@@ -24,7 +22,7 @@ app.get('/', function(req,res){
     res.sendfile("/hw/public/index.html");
 })
 
-// query 功能
+// get
 app.get('/query', function(req, res) {
 
     res.header("Access-Control-Allow-Origin", "*");
@@ -32,26 +30,9 @@ app.get('/query', function(req, res) {
     var response = {
         result: true,
         data: []
-        /* data: [{
-                id: 0,
-                name: "小米路由器",
-                price: 399,
-                count: 1,
-                image: 'http://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1490332273.78529474.png?width=160&height=160'
-            },
-            {
-                id: 1,
-                name: "米家全景相機",
-                price: 7995,
-                count: 1,
-                image: 'http://i01.appmifile.com/f/i/g/2016overseas/mijiaquanjingxiangji800.png?width=160&height=160'
-            }
-        ] */
     }
 
-    
-    // TODO 作業二 - 查詢資料       
-    // 請將查詢mongodb的程式碼寫在這裡，並改寫下方response，使得查詢結果能送至前端
+    // connect mongodb to get all records
     MongoClient.connect(url, function(err, dbo) {
         if (err) {
             response.result = false
@@ -65,24 +46,19 @@ app.get('/query', function(req, res) {
 
         db.collection("products").find({}).toArray(function(err, result) {
            if (err) throw err;
-           //console.log(response.data)
            for(var i=0; i<result.length; i++){
                response.data.push(result[i])
            }
-           // console.log(response.data);
            db.close();
            console.log(response.data.length);
            res.json(response)
            return
         }); 
-//
-        //console.log(response.data.length);
-        //res.json(response)
     })
 })
 
 
-//insert功能
+//insert
 app.post('/insert', function(req, res) {
     var data = {
         name: req.body.name,
@@ -91,16 +67,13 @@ app.post('/insert', function(req, res) {
         image: req.body.image
     }
 
-    // TODO 作業二 - 新增資料
-    // 請將新增資料的程式碼寫在，使得將client送過來的 data 能寫入至 mongodb 中
+    // connect to Mongodb
     MongoClient.connect(url,function(err,db){
         if(err){
             res.message = "insert err" + err;
             return ;
         }
-////////
         db.collection("products").insert(data) ;
-////////    
     });
 
     var response = {
@@ -110,15 +83,13 @@ app.post('/insert', function(req, res) {
     res.json(response)
 })
 
-//update功能
+//update
 app.post('/update', function(req, res) {
     var data = {
         _id: req.body._id,
         name: req.body.name,
         price: req.body.price,
     }
-
-
     var response = {
         result: true,
         data: data
@@ -132,10 +103,7 @@ app.post('/update', function(req, res) {
             return
         }
 
-        // TODO 作業二 - 更新資料
-        // 將mongoDB資料中對應的 data.id 找出來，並更新其 name 和 price 資料
-        // https://docs.mongodb.com/manual/tutorial/update-documents/
-
+        // update the record with the same id
         var filter = {
             _id: objectID(data._id)
         }
@@ -143,14 +111,14 @@ app.post('/update', function(req, res) {
             name: data.name,
             price: data.price
         }
-    /////
         db.collection("products").update(filter,{$set:update});
-    /////
+        db.close()
+        res.json(response)
+        return
     });
-
 })
 
-// delete功能
+// delete
 app.post('/delete', function(req, res) {
     var data = {
         _id: req.body._id,
@@ -168,24 +136,20 @@ app.post('/delete', function(req, res) {
             res.json(response)
             return
         }
-        // TODO 作業二 - 刪除資料
-        // 將ID 的資料 從mongodb中刪除
-        // https://docs.mongodb.com/manual/tutorial/remove-documents/
 
-        // 查詢要刪除的ID
+        // delete the records with the same id
         var filter = {
             _id: objectID(data._id)
         }
-        /////
         db.collection("products").remove(filter);
-        /////
         db.close()
+        res.json(response)
         return
     })
 
 })
 
-// 啟動且等待連接
+// start the server
 app.listen(app.get('port'), function() {
     console.log('Server running at http://127.0.0.1:' + app.get('port'))
 });
